793d2014a663fc2e22ad9ba4b9f9d839f0041210
furaizi <furachannel16@gmail.com>
2025-03-17 19:00:57 +0200

    Add tests for CalculatePostfix function (simple cases)

--- implementation_test.go
+++ implementation_test.go
@@ -5,13 +5,37 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := CalculatePostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+func TestCalculatePostfix_Addition(t *testing.T) {
+	res, err := CalculatePostfix("1 2 +")
+	require.Nil(t, err)
+	assert.Equal(t, "3", res)
+}
+
+func TestCalculatePostfix_Multiplication(t *testing.T) {
+	res, err := CalculatePostfix("3 6 *")
+	require.Nil(t, err)
+	assert.Equal(t, "18", res)
+}
+
+func TestCalculatePostfix_Subtraction(t *testing.T) {
+	res, err := CalculatePostfix("5 3 -")
+	require.Nil(t, err)
+	assert.Equal(t, "2", res)
+}
+
+func TestCalculatePostfix_Division(t *testing.T) {
+	res, err := CalculatePostfix("-8 2 /")
+	require.Nil(t, err)
+	assert.Equal(t, "-4", res)
+}
+
+func TestCalculatePostfix_Power(t *testing.T) {
+	res, err := CalculatePostfix("4 3 ^")
+	require.Nil(t, err)
+	assert.Equal(t, "64", res)
 }
 
 func ExampleCalculatePostfix() {
