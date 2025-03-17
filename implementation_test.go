d11b4fd4ab52357fca6e6637c6c42690f8575472
furaizi <furachannel16@gmail.com>
2025-03-17 20:34:58 +0200

    Refactor tests for CalculatePostfix function

--- implementation_test.go
+++ implementation_test.go
@@ -8,96 +8,79 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCalculatePostfix_Addition(t *testing.T) {
-	res, err := CalculatePostfix("1 2 +")
-	require.Nil(t, err)
-	assert.Equal(t, "3", res)
+type TestCase struct {
+	name     string
+	input    string
+	expected string
 }
 
-func TestCalculatePostfix_Multiplication(t *testing.T) {
-	res, err := CalculatePostfix("3 6 *")
-	require.Nil(t, err)
-	assert.Equal(t, "18", res)
-}
+func TestCalculatePostfix_SimpleCases(t *testing.T) {
+	tests := []TestCase{
+		{"Addition", "1 2 +", "3"},
+		{"Multiplication", "3 6 *", "18"},
+		{"Subtraction", "5 3 -", "2"},
+		{"Division", "-8 2 /", "-4"},
+		{"Power", "4 3 ^", "64"},
+	}
 
-func TestCalculatePostfix_Subtraction(t *testing.T) {
-	res, err := CalculatePostfix("5 3 -")
-	require.Nil(t, err)
-	assert.Equal(t, "2", res)
-}
-
-func TestCalculatePostfix_Division(t *testing.T) {
-	res, err := CalculatePostfix("-8 2 /")
-	require.Nil(t, err)
-	assert.Equal(t, "-4", res)
-}
-
-func TestCalculatePostfix_Power(t *testing.T) {
-	res, err := CalculatePostfix("4 3 ^")
-	require.Nil(t, err)
-	assert.Equal(t, "64", res)
-}
-
-func TestCalculatePostfix_Medium(t *testing.T) {
-	// (1 + 2) * (3 - 4) / 5
-	res, err := CalculatePostfix("1 2 + 3 4 - * 5 /")
-	require.Nil(t, err)
-	assert.Equal(t, "-0.6", res)
-}
-
-func TestCalculatePostfix_Hard(t *testing.T) {
-	// 42 / 6 + (70 - 64) * (2 ^ 2) + 52 / 26 - 70 / 5
-	res, err := CalculatePostfix("42 6 / 70 64 - 2 2 ^ * + 52 25 / + 70 5 / -")
-	require.Nil(t, err)
-	assert.Equal(t, "19", res)
-}
-
-func TestCalculatePostfix_Complex(t *testing.T) {
-	// 5 + (6 * (2 ^ 3 - 4)) / (3 - 1)
-	res, err := CalculatePostfix("5 6 2 3 ^ 4 - * 3 1 - / +")
-	require.Nil(t, err)
-	assert.Equal(t, "17", res)
-}
-
-func TestCalculatePostfix_EmptyString(t *testing.T) {
-	_, err := CalculatePostfix("")
-	if assert.NotNil(t, err) {
-		assert.ErrorContains(t, err, emptyStringErrorMessage)
+	for _, test := range tests {
+		// capturing the loop variable
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			res, err := CalculatePostfix(test.input)
+			require.NoError(t, err, "Unexpected error for input string %q", test.input)
+			assert.Equal(t, test.expected, res, "Invalid result for input string %q", test.input)
+		})
 	}
 }
 
-func TestCalculatePostfix_NotEnoughOperands(t *testing.T) {
-	_, err := CalculatePostfix("9 -")
-	if assert.NotNil(t, err) {
-		assert.ErrorContains(t, err, incorrectNumberOfOperandsErrorMessage)
+func TestCalculatePostfix_ComplexCases(t *testing.T) {
+	tests := []TestCase{
+		// (1 + 2) * (3 - 4) / 5
+		{"Medium", "1 2 + 3 4 - * 5 /", "-0.6"},
+		// 42 / 6 + (70 - 64) * (2 ^ 2) + 52 / 26 - 70 / 5
+		{"Hard", "42 6 / 70 64 - 2 2 ^ * + 52 25 / + 70 5 / -", "19"},
+		// 5 + (6 * (2 ^ 3 - 4)) / (3 - 1)
+		{"Complex", "5 6 2 3 ^ 4 - * 3 1 - / +", "17"},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			res, err := CalculatePostfix(test.input)
+			require.NoError(t, err, "Unexpected error for input string %q", test.input)
+			assert.Equal(t, test.expected, res, "Invalid result for input string %q", test.input)
+		})
 	}
 }
 
-func TestCalculatePostfix_TooManyOperands(t *testing.T) {
-	_, err := CalculatePostfix("5 1 2 *")
-	if assert.NotNil(t, err) {
-		assert.ErrorContains(t, err, incorrectNumberOfOperandsErrorMessage)
+func TestCalculatePostfix_ExceptionalCases(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       string
+		expectedErr string
+	}{
+		{"EmptyString", "", emptyStringErrorMessage},
+		{"NotEnoughOperands", "9 -", incorrectNumberOfOperandsErrorMessage},
+		{"TooManyOperands", "5 1 2 *", incorrectNumberOfOperandsErrorMessage},
+		{"UnknownOperator", "9 2 &", unsupportedSymbolErrorMessage},
+		{"UnknownOperand", "9 b +", unsupportedSymbolErrorMessage},
 	}
-}
 
-func TestCalculatePostfix_UnknownOperator(t *testing.T) {
-	_, err := CalculatePostfix("9 2 &")
-	if assert.NotNil(t, err) {
-		assert.ErrorContains(t, err, unsupportedSymbolErrorMessage)
-	}
-}
-
-func TestCalculatePostfix_UnknownOperand(t *testing.T) {
-	_, err := CalculatePostfix("9 b +")
-	if assert.NotNil(t, err) {
-		assert.ErrorContains(t, err, unsupportedSymbolErrorMessage)
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			_, err := CalculatePostfix(test.input)
+			require.Error(t, err, "Error expected for input string %q", test.input)
+			assert.ErrorContains(t, err, test.expectedErr, "Error message does not match expectation for input string %q", test.input)
+		})
 	}
 }
 
 func ExampleCalculatePostfix() {
-	res, _ := CalculatePostfix("+ 2 2")
+	res, _ := CalculatePostfix("5 2 +")
 	fmt.Println(res)
-
-	// Output:
-	// 2 2 +
 }
