e6832a258e17274fafde536ba0b40fd30e995a6b
furaizi <furachannel16@gmail.com>
2025-03-17 18:45:22 +0200

    Rename PrefixToPostfix function to CalculatePostfix

--- implementation_test.go
+++ implementation_test.go
@@ -8,14 +8,14 @@ import (
 )
 
 func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+	res, err := CalculatePostfix("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
 		assert.Equal(t, "4 2 - 3 * 5 +", res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExampleCalculatePostfix() {
+	res, _ := CalculatePostfix("+ 2 2")
 	fmt.Println(res)
 
 	// Output:
