d8a9c65c6f40bc79e85fd599df77b766e6cadfee
furaizi <furachannel16@gmail.com>
2025-03-17 19:52:47 +0200

    Add tests for CalculatePostfix function (exceptional cases)

--- implementation.go
+++ implementation.go
@@ -2,6 +2,10 @@ package lab2
 
 import "fmt"
 
+var emptyStringErrorMessage = "Error: empty input"
+var unsupportedSymbolErrorMessage = "Error: unsupported symbol"
+var incorrectNumberOfOperandsErrorMessage = "Error: incorrect number of operands"
+
 // TODO: document this function.
 // CalculatePostfix converts
 func CalculatePostfix(input string) (string, error) {
--- implementation_test.go
+++ implementation_test.go
@@ -59,6 +59,41 @@ func TestCalculatePostfix_Complex(t *testing.T) {
 	assert.Equal(t, "17", res)
 }
 
+func TestCalculatePostfix_EmptyString(t *testing.T) {
+	_, err := CalculatePostfix("")
+	if assert.NotNil(t, err) {
+		assert.ErrorContains(t, err, emptyStringErrorMessage)
+	}
+}
+
+func TestCalculatePostfix_NotEnoughOperands(t *testing.T) {
+	_, err := CalculatePostfix("9 -")
+	if assert.NotNil(t, err) {
+		assert.ErrorContains(t, err, incorrectNumberOfOperandsErrorMessage)
+	}
+}
+
+func TestCalculatePostfix_TooManyOperands(t *testing.T) {
+	_, err := CalculatePostfix("5 1 2 *")
+	if assert.NotNil(t, err) {
+		assert.ErrorContains(t, err, incorrectNumberOfOperandsErrorMessage)
+	}
+}
+
+func TestCalculatePostfix_UnknownOperator(t *testing.T) {
+	_, err := CalculatePostfix("9 2 &")
+	if assert.NotNil(t, err) {
+		assert.ErrorContains(t, err, unsupportedSymbolErrorMessage)
+	}
+}
+
+func TestCalculatePostfix_UnknownOperand(t *testing.T) {
+	_, err := CalculatePostfix("9 b +")
+	if assert.NotNil(t, err) {
+		assert.ErrorContains(t, err, unsupportedSymbolErrorMessage)
+	}
+}
+
 func ExampleCalculatePostfix() {
 	res, _ := CalculatePostfix("+ 2 2")
 	fmt.Println(res)
